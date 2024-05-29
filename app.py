351c7fe7e81a0a0ed0b1668dfa07e961abc736e7
Mojeeb2002 <mojeebdaffallah38@gmail.com>
2024-05-29 13:53:45 +0300

    testing

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def user(name):
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
+#Internal Server Error
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
