81b1f283c97d5c5450cc68b879e23f515e1b55a7
Mojeeb2002 <mojeebdaffallah38@gmail.com>
2024-05-29 13:46:38 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+
+
+app = Flask(__name__)
+
+@app.route('/home')
+@app.route('/')
+def index():
+    first_name = 'Mojeeb'
+    return render_template('index.html', first_name=first_name)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', user_name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
